e7f7d505dffa578864b5eb67110b929b74b4ebe3
Dhirendra Kaswa <dhirendrakaswa@Dhirendras-MacBook-Air.local>
2023-08-06 14:19:57 +0530

    Minor Changes

--- lecture-5/tutorial-5/src/NumberSystem.java
+++ lecture-5/tutorial-5/src/NumberSystem.java
@@ -2,15 +2,5 @@ import java.util.Scanner;
 
 public class NumberSystem {
     public static void main(String[] args) {
-        Scanner input= new Scanner(System.in);
-        System.out.println("Please enter decimal number: ");
-        int dec= input.nextInt();
-        rem=0;
-        sum=0;
-
-        while(dec>0){
-            rem=dec%2;
-            sum=sum*pow(10,i)
-        }
     }
 }
