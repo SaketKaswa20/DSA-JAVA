e55d94902c17c38ed488f1025bd0f7537fe9c9ed
Dhirendra Kaswa <dhirendrakaswa@Dhirendras-MacBook-Air.local>
2023-08-06 19:51:14 +0530

    Reversing Array

---
+++ lecture-5/tutorial-5/src/ReverseArray.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6};
+        Rev(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+//Doing Something Wrong ig
+    static void Rev(int[] arr){
+        for (int i = 0; i <= (arr.length-1)/2; i++) {
+            int temp=arr[i];
+            arr[i]=arr[arr.length-1];
+            arr[arr.length-1]=temp;
+        }
+    }
+}
