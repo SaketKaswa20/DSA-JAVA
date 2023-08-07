9adf5417cd73b4471e067502763050643900b697
Saket Kaswa <kaswasd21.mfg@coep.ac.in>
2023-08-07 20:21:03 +0530

    Search in 2D Array

--- lecture-6/tutorial-6/src/SearchIn2DArray.java
+++ lecture-6/tutorial-6/src/SearchIn2DArray.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class SearchIn2DArray {
     public static void main(String[] args) {
         int[][] arr= {{1,2,3},{4,5,6}};
-        int target=4;
+        int target=6;
         int[] ans= Find(arr,target);
         System.out.println(Arrays.toString(ans));
     }
