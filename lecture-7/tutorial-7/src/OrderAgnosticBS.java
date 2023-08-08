2145536e6eba853647c4a04f86aa596193c2a656
Saket Kaswa <kaswasd21.mfg@coep.ac.in>
2023-08-09 00:03:26 +0530

    Order Agnostics

--- lecture-7/tutorial-7/src/OrderAgnosticBS.java
+++ lecture-7/tutorial-7/src/OrderAgnosticBS.java
@@ -2,7 +2,7 @@ public class OrderAgnosticBS {
     public static void main(String[] args) {
         int[] arr={1,2,3,4,5,6,7,8,9};
         int target=7;
-        System.out.println(binarySearch(arr,target));
+        System.out.println(orderAgnosticBS(arr,target));
     }
 
     static int orderAgnosticBS(int[] arr,int target){
@@ -30,7 +30,7 @@ public class OrderAgnosticBS {
                     start=mid+1;
                 }
             }
-            return -1;
         }
+        return -1;
     }
 }
