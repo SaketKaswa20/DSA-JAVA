e4a0fdc6b99dcd9fd8d6b45ee7e34ddafa1b02aa
Saket Kaswa <kaswasd21.mfg@coep.ac.in>
2023-08-29 20:36:52 +0530

    Smallest Letter

---
+++ lecture-8/tutorial-8/src/SmallestLetter.java
@@ -0,0 +1,23 @@
+public class SmallestLetter {
+//    https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+    public static void main(String[] args) {
+
+    }
+
+    public char nextGreatestLetter(char[] letters, char target) {
+        static int Ceiling(int[] letters, int target){
+            int start=0;
+            int end= letters.length-1;
+            while (start<=end){
+//            int mid= start+end/2; since start+end can surpass the int limit range
+                int mid= start+(end-start)/2;
+
+                if (target<letters[mid]){
+                    end= mid-1;
+                } else {
+                    start=mid+1;
+                }
+            }
+            return letters[start % letters.length];
+    }
+}
