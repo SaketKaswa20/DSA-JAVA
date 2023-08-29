cc87c3a3373ebcd86d4f31c8545a49df11562465
Saket Kaswa <kaswasd21.mfg@coep.ac.in>
2023-08-29 23:45:00 +0530

    My Solution to Leetcode Problem

---
+++ lecture-8/tutorial-8/src/FirstandLastPositioninSortedArray.java
@@ -0,0 +1,25 @@
+public class FirstandLastPositioninSortedArray {
+    // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/submissions/
+    //My Leetcode Submission
+//    class Solution {
+//        public int[] searchRange(int[] nums, int target) {
+//            int[] position=new int[2];
+//            for(int i=0; i<nums.length; i++){
+//                if(nums[i]==target){
+//                    position[0]=i;
+//                    for(int j=i; j<nums.length; j++){
+//                        if(nums[j]==target){
+//                            position[1]=j;
+//                        }
+//                    }
+//                    return position;
+//                }
+//            }
+//            position[0]=-1;
+//            position[1]=-1;
+//            return position;
+//        }
+//    }
+
+
+}
