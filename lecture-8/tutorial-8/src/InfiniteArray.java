1a534569467b8780f8cc32b9d95b6826a7dbd9ce
Saket <kaswasd21.mfg@coeptech.ac.in>
2023-09-02 18:29:57 +0530

    Infinite Array

---
+++ lecture-8/tutorial-8/src/InfiniteArray.java
@@ -0,0 +1,43 @@
+// https://www.geeksforgeeks.org/find-position-element-sorted-array-infinite-numbers/
+
+public class InfiniteArray {
+    public static void main(String[] args) {
+        int[] arr= {3,5,7,9,10,90,100,130,140,160,170};
+        int target= 10;
+        System.out.println(ans(arr,target));
+    }
+
+    static int ans(int[] arr, int target){
+        //first find the range
+        //start with box of size 2
+        int start=0;
+        int end=1;
+
+        //condition for the target to lie in the range
+        while(target>arr[end]){
+            int newstart= end+1;
+            //double the box value
+            //end= previous end+ sizeofbox*2
+            end= end+ (end-start+1)*2;
+            start=newstart;
+        }
+        return BinarySearch(arr, target, start, end);
+    }
+
+    static int BinarySearch(int[] arr, int target, int start, int end){
+        while (start<=end){
+//            int mid= start+end/2; since start+end can surpass the int limit range
+            int mid= start+(end-start)/2;
+
+            if (target<arr[mid]){
+                end= mid-1;
+            } else if (target>arr[mid]) {
+                start=mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
+
