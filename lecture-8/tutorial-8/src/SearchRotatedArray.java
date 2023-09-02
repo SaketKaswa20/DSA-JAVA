632f2c333728c7ef3d7f48fa36e57c7062471a65
Saket Kaswa <kaswasd21.mfg@coep.ac.in>
2023-09-02 23:53:19 +0530

    Search in Rotated Array

---
+++ lecture-8/tutorial-8/src/SearchRotatedArray.java
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+public class SearchRotatedArray {
+    //Approach: 1. Find Pivot [Largest Element]
+    //2. Search in first half[Using Binary Search] [start  to pivot]
+    //3. Search in another half[Using Binary Search] [pivot+1 to end]
+
+}
