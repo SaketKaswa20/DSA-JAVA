cb004e475af9761360cfb27dfdfdbd4d95700e8d
Saket Kaswa <kaswasd21.mfg@coep.ac.in>
2023-09-05 07:00:35 +0530

    Kids with greater Number of Candies -LeetCode

--- lecture-6/tutorial-6/src/GreaterNumberofCandies.java
+++ lecture-6/tutorial-6/src/GreaterNumberofCandies.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+//https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
 public class GreaterNumberofCandies {
     public static void main(String[] args) {
         int[] candies ={2,3,5,1,3};
