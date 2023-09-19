271046124cc70c892d4e395fca71af0e9ca5cede
Saket Kaswa <kaswasd21.mfg@coep.ac.in>
2023-09-20 00:36:25 +0530

    Operators in Strings

---
+++ lecture-11/tutorial-11/src/Operators.java
@@ -0,0 +1,9 @@
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println('a'+'b'); //Output 195
+        System.out.println("a"+"b"); //Output ab
+        System.out.println('a'+3); //Output 100
+        System.out.println((char)('a'+3)); //Output d
+        System.out.println("a"+1); //Output a1
+    }
+}
