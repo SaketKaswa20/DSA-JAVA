9b304691951d8fa438c9354cc457977f4bf8dc0e
Saket Kaswa <kaswasd21.mfg@coep.ac.in>
2023-09-20 00:12:44 +0530

    Pretty Printing

--- lecture-11/tutorial-11/src/Main.java
+++ lecture-11/tutorial-11/src/Main.java
@@ -8,5 +8,6 @@ public class Main {
         String name2= new String("Saket");
         System.out.println(name1.equals(name2));
         System.out.println(name1==name2); //this will give false because we have explicitly mentioned new keyword
+        System.out.println(name1.charAt(0)); //This will print S
     }
 }
---
+++ lecture-11/tutorial-11/src/PrettyPrinting.java
@@ -0,0 +1,12 @@
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a= 453.1234f;
+        //System.out.printf("Formatted number is %.2f", a); //Will print numbers upto 2 decimals and also even round off
+//        System.out.println(Math.PI); //This will print 3.141592653589793
+//        System.out.printf("PI is %.3f", Math.PI); //This will print 3.142
+        System.out.printf("My name is %s and surname is %s", "Saket", "Kaswa");
+        //Output is My name is Saket and surname is Kaswa
+        //Search for Placeholder in Java Strings
+
+    }
+}
