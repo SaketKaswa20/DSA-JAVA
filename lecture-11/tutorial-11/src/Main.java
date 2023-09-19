b4b260140deb893abeacb3501f608d370270a98e
Saket Kaswa <kaswasd21.mfg@coep.ac.in>
2023-09-19 16:32:44 +0530

    Explicit Mentioning New String

--- lecture-11/tutorial-11/src/Main.java
+++ lecture-11/tutorial-11/src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         String name1= new String("Saket");
         String name2= new String("Saket");
         System.out.println(name1.equals(name2));
+        System.out.println(name1==name2); //this will give false because we have explicitly mentioned new keyword
     }
 }
