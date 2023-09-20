04be04a0d6e9f7e972d442ea7d83ce45d7f41e5f
Saket Kaswa <kaswasd21.mfg@coep.ac.in>
2023-09-20 18:56:18 +0530

    String Methods

--- lecture-11/tutorial-11/src/Methods.java
+++ lecture-11/tutorial-11/src/Methods.java
@@ -4,5 +4,9 @@ public class Methods {
     public static void main(String[] args) {
         String name= "Saket Kaswa";
         System.out.println(Arrays.toString(name.toCharArray()));
+        System.out.println(name.toLowerCase());
+        System.out.println(name.indexOf('a'));
+        System.out.println("   Saket   ".strip());
+        System.out.println(Arrays.toString(name.split(" ")));
     }
 }
