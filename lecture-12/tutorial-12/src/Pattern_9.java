708ff6c04671d70b283393d49840f47239c34367
Saket Kaswa <kaswasd21.mfg@coep.ac.in>
2023-09-21 20:18:02 +0530

    Pattern-9

---
+++ lecture-12/tutorial-12/src/Pattern_9.java
@@ -0,0 +1,17 @@
+public class Pattern_9 {
+    public static void main(String[] args) {
+        pattern(4);
+    }
+    
+    static void pattern(int n){
+        int OriginalN=n;
+        n=2*n;
+        for (int row = 0; row <= n; row++) {
+            for (int col = 0; col <= n; col++) {
+                int atEveryIndex=OriginalN-Math.min(Math.min(row,col), Math.min(n-row,n-col));
+                System.out.print(atEveryIndex+" ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
