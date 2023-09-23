35c9fc5d62bdc1ed727fd8962e05e429f21196f7
SaketKaswa20 <kaswasd21.mfg@coep.ac.in>
2023-09-23 21:54:42 +0530

    Fibonacci Series using Recursion

---
+++ lecture-13/tutorial-13/src/Fibonacci_Number.java
@@ -0,0 +1,12 @@
+public class Fibonacci_Number {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(5));
+    }
+
+    static int fibonacci(int n){
+        if (n<2){
+            return n;
+        }
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
